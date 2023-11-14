33e45280a637fe6421da0eb67cd05d04099cd9ab
leunghon <leunghon5@gmail.com>
2023-11-13 17:08:42 -0800

    updated code

---
+++ trailxdetection.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import sys
+import argparse
+
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput, Log
+from datetime import datetime
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=detectNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# create video sources and outputs
+input = videoSource(args.input, argv=sys.argv)
+output = videoOutput(args.output, argv=sys.argv)
+	
+# load the object detection network
+net = detectNet(args.network, sys.argv, args.threshold)
+
+# note: to hard-code the paths to load a model, the following API can be used:
+#
+# net = detectNet(model="model/ssd-mobilenet.onnx", labels="model/labels.txt", 
+#                 input_blob="input_0", output_cvg="scores", output_bbox="boxes", 
+#                 threshold=args.threshold)
+
+# process frames until EOS or the user exits
+while True:
+    # capture the next image
+    img = input.Capture()
+
+    if img is None: # timeout
+        continue  
+        
+    # detect objects in the image (with overlay)
+    detections = net.Detect(img, overlay=args.overlay)
+
+    # Count the number of persons detected
+    person_count = sum(1 for d in detections if d.ClassID == 1)  # Assuming '1' is the class ID for persons
+
+    # Get the current timestamp
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Print or log the count with timestamp
+    print(f"{current_time}: Detected {person_count} persons")
+
+    # Append the count and timestamp to a file
+    with open("person_count_log.txt", "a") as file:
+        file.write(f"{current_time}: {person_count} persons detected\n")
+
+
+    # print the detections
+    print("detected {:d} objects in image".format(len(detections)))
+
+    for detection in detections:
+        print(detection)
+
+    # render the image
+    output.Render(img)
+
+    # update the title bar
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(args.network, net.GetNetworkFPS()))
+
+    # print out performance info
+    net.PrintProfilerTimes()
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
